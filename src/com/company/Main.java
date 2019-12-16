6376dfed0bd332110186c56db83957a59c791964
Student <Student@EPAMYERW0046.moscow.epam.com>
2019-12-16 17:02:06 +0400

    Rebase situation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int [] array= {12, 3, 5, 67, 87};
+	int [] array= {12, 3, 5, 67, 87, 89, 34, 56, 23};
 	int max=array[0];
 	for(int i=1; i<array.length; i++){
 	    if(array[i]>max){
@@ -16,3 +16,4 @@ public class Main {
 //I like it. Good job, Lusine!
 //Created conflict!!!rgkjeghkhoer123qe12e12e2e
 //My last comment
+//New comment
