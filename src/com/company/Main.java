3ebbc67321df335eaffbde2e71b1d93f744c80d9
Student <Student@EPAMYERW0046.moscow.epam.com>
2019-12-16 14:07:48 +0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] array= {12, 3, 5, 67, 87};
+	int max=array[0];
+	for(int i=1; i<array.length; i++){
+	    if(array[i]>max){
+	        max=array[i];
+        }
+    }
+	System.out.println(max);
+    }
+}
